eed8d2cbe33231a259308aed2ea9f376b7f0347e
zlgodick <102632790+zlgodick@users.noreply.github.com>
2023-11-30 23:09:17 -0800

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import numpy as np
+import csv
+import os
+
+desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+file_path = os.path.join(desktop_path, "Train.csv")
+with open(file_path, mode='r') as csv_file:
+    read = csv.reader(csv_file)
+    matrix = [row for row in read]
+X = pd.DataFrame(matrix)
+X.head()
+
 
